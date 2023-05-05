dd955213f89689f08e828b4fc2a81d55b1df5571
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2023-05-05 10:09:49 +0200

    update case1

--- examples/case1/main.py
+++ examples/case1/main.py
@@ -8,53 +8,13 @@ import sys
 
 sys.path.insert(0, "src/")
 from hodge_solver import HodgeSolver
-from sampler import Sampler
+from sampler import SamplerR, SamplerQ
 from setup import *
 
 
-def visualize(mdg, mu, r, sampler, file_name):
-    # visualization of the results
-    q_f = sampler.get_q_f(mu)
-    q, p = sampler.hs.step3(q_f, r)
-
-    # post process of r
-    if mdg.dim_max() == 2:
-        proj_r = pg.eval_at_cell_centers(mdg, pg.Lagrange1(sampler.keyword))
-        cell_r = proj_r @ r
-    if mdg.dim_max() == 3:
-        proj_r = pg.eval_at_cell_centers(mdg, pg.Nedelec0(sampler.keyword))
-        cell_r = (proj_r @ r).reshape((3, -1), order="F")
-
-    # post process velocity
-    proj_q = pg.eval_at_cell_centers(mdg, pg.RT0(sampler.keyword))
-    cell_q = (proj_q @ q).reshape((3, -1), order="F")
-
-    # post process pressure
-    proj_p = pg.eval_at_cell_centers(mdg, pg.PwConstants(sampler.keyword))
-    cell_p = proj_p @ p
-
-    # save the solutions to be exported in the data dictionary of the mdg
-    for _, data in mdg.subdomains(return_data=True):
-        pp.set_solution_values("cell_r", cell_r, data, 0)
-        pp.set_solution_values("cell_q", cell_q, data, 0)
-        pp.set_solution_values("cell_p", cell_p, data, 0)
-
-    # export the solutions
-    save = pp.Exporter(mdg, file_name)
-    save.write_vtu(["cell_r", "cell_q", "cell_p"])
-
-
-def main():
-    mdg = pg.grid_unitary(2, 0.125)
-    mdg.compute_geometry()
-
-    create_data(mdg)
-
-    keyword = "flow"
+def main_sampler_r(mdg, keyword, num_samples):
     hs = HodgeSolver(mdg, pg.RT0(keyword))
-    sampler = Sampler(hs)
-
-    num_samples = 4
+    sampler = SamplerR(mdg, keyword, hs)
 
     r_samples = np.empty(num_samples, dtype=np.ndarray)
     mu_samples = np.empty((num_samples, 2))
@@ -70,27 +30,35 @@ def main():
     loss = sampler.compute_loss(mu_samples[0, :], r_samples[0, :], r_rand)
     print(loss)
     print(mu_samples)
-    visualize(mdg, mu_samples[0, :], r_samples[0, :], sampler, "sol")
+    sampler.visualize(mu_samples[0, :], r_samples[0, :], "sol")
 
-    # return
 
-    # r, q_f = sampler.get_sample([0, 1])
+def main_sampler_q(mdg, keyword, num_samples):
+    sampler = SamplerQ(mdg, keyword)
 
-    # face_mass = pg.face_mass(mdg)
-    # div = pg.cell_mass(mdg) @ pg.div(mdg)
-    # spp = sps.bmat([[face_mass, -div.T], [div, None]]).tocsc()
-    # rhs = np.hstack((g, f))
+    q0_samples = np.empty(num_samples, dtype=np.ndarray)
+    mu_samples = np.empty((num_samples, 2))
+    for idx, (mu, q0) in enumerate(sampler.generate_set(num_samples, seed=1)):
+        mu_samples[idx, :] = mu
+        q0_samples[idx] = q0
 
-    # qp_ref = sps.linalg.spsolve(spp, rhs)
-    # q_ref = qp_ref[: g.size]
-    # p_ref = qp_ref[g.size :]
+    q0_samples = np.vstack(q0_samples)
 
-    # err_p = p - p_ref
-    # err_q = q - q_ref
+    r_rand = np.random.rand(mdg.num_subdomain_ridges())
 
-    # print("err_p:", np.sqrt(err_p @ cell_mass @ err_p))
-    # print("err_q:", np.sqrt(err_q @ face_mass @ err_q))
+    loss = sampler.compute_loss(mu_samples[0, :], q0_samples[0, :], r_rand)
+    print(loss)
+    print(mu_samples)
+    sampler.visualize(mu_samples[0, :], q0_samples[0, :], "sol")
 
 
 if __name__ == "__main__":
-    main()
+    mdg = pg.grid_unitary(2, 0.125)
+    mdg.compute_geometry()
+
+    create_data(mdg)
+
+    keyword = "flow"
+    num_samples = 4
+
+    main_sampler_q(mdg, keyword, num_samples)
--- examples/case1/sampler.py
+++ examples/case1/sampler.py
@@ -1,17 +1,18 @@
 import numpy as np
 from scipy.stats import qmc
+import scipy.sparse as sps
 
 import porepy as pp
 import pygeon as pg
 
 
 class Sampler:
-    def __init__(self, hs):
-        self.keyword = hs.discr.keyword
+    def __init__(self, mdg, keyword):
+        self.l_bounds = [0, 0]
+        self.u_bounds = [4, 4]
 
-        self.hs = hs
-        self.mdg = hs.mdg
-
+        self.mdg = mdg
+        self.keyword = keyword
         self.cell_mass = pg.cell_mass(self.mdg)
         self.face_mass = pg.face_mass(self.mdg)
 
@@ -27,6 +28,13 @@ class Sampler:
     def get_g(self):
         return np.zeros(self.mdg.num_subdomain_faces())
 
+
+class SamplerR(Sampler):
+    def __init__(self, mdg, keyword, hs):
+        super().__init__(mdg, keyword)
+
+        self.hs = hs
+
     def get_q_f(self, mu):
         f = self.get_f(mu)
         return self.hs.step1(f)
@@ -39,10 +47,7 @@ class Sampler:
     def generate_set(self, num, seed=None):
         lhc = qmc.LatinHypercube(d=2, seed=seed)
         mu_samples = lhc.random(num)
-
-        l_bounds = [0, 0]
-        u_bounds = [4, 4]
-        mu_samples = qmc.scale(mu_samples, l_bounds, u_bounds)
+        mu_samples = qmc.scale(mu_samples, self.l_bounds, self.u_bounds)
 
         for mu in mu_samples:
             yield mu, self.get_r(mu)
@@ -65,3 +70,109 @@ class Sampler:
         loss_p = self.compute_loss_p(mu, r_true, r)
 
         return weights[0] * loss_r + weights[1] * loss_p
+
+    def visualize(self, mu, r, file_name):
+        # visualization of the results
+        q_f = self.get_q_f(mu)
+        q, p = self.hs.step3(q_f, r)
+
+        # post process of r
+        if self.mdg.dim_max() == 2:
+            proj_r = pg.eval_at_cell_centers(self.mdg, pg.Lagrange1(self.keyword))
+            cell_r = proj_r @ r
+        if self.mdg.dim_max() == 3:
+            proj_r = pg.eval_at_cell_centers(self.mdg, pg.Nedelec0(self.keyword))
+            cell_r = (proj_r @ r).reshape((3, -1), order="F")
+
+        # post process velocity
+        proj_q = pg.eval_at_cell_centers(self.mdg, pg.RT0(self.keyword))
+        cell_q = (proj_q @ q).reshape((3, -1), order="F")
+
+        # post process pressure
+        proj_p = pg.eval_at_cell_centers(self.mdg, pg.PwConstants(self.keyword))
+        cell_p = proj_p @ p
+
+        # save the solutions to be exported in the data dictionary of the mdg
+        for _, data in self.mdg.subdomains(return_data=True):
+            pp.set_solution_values("cell_r", cell_r, data, 0)
+            pp.set_solution_values("cell_q", cell_q, data, 0)
+            pp.set_solution_values("cell_p", cell_p, data, 0)
+
+        # export the solutions
+        save = pp.Exporter(self.mdg, file_name)
+        save.write_vtu(["cell_r", "cell_q", "cell_p"])
+
+
+class SamplerQ(Sampler):
+    def __init__(self, mdg, keyword):
+        super().__init__(mdg, keyword)
+
+        self.swp = pg.Sweeper(mdg)
+
+        div = self.cell_mass @ pg.div(mdg)
+        self.spp = sps.bmat([[self.face_mass, -div.T], [div, None]]).tocsc()
+        self.curl_op = pg.curl(mdg)
+
+    def get_q0(self, mu):
+        f = self.get_f(mu)
+        g = self.get_g()
+
+        q_f = self.swp.sweep(f)
+
+        rhs = np.hstack((g, f))
+        qp = sps.linalg.spsolve(self.spp, rhs)
+        return qp[: self.face_mass.shape[0]] - q_f
+
+    def generate_set(self, num, seed=None):
+        lhc = qmc.LatinHypercube(d=2, seed=seed)
+        mu_samples = lhc.random(num)
+        mu_samples = qmc.scale(mu_samples, self.l_bounds, self.u_bounds)
+
+        for mu in mu_samples:
+            yield mu, self.get_q0(mu)
+
+    def compute_loss_r(self, q0_true, r):
+        diff = q0_true - self.curl_op @ r
+        return np.sqrt(diff @ self.face_mass @ diff)
+
+    def compute_loss_p(self, mu, q0_true, r):
+        f = self.get_f(mu)
+
+        q_f = self.swp.sweep(f)
+        p_true = self.swp.sweep_transpose(self.face_mass @ (q_f + q0_true))
+        p = self.swp.sweep_transpose(self.face_mass @ (q_f + self.curl_op @ r))
+
+        diff = p_true - p
+
+        return np.sqrt(diff @ self.cell_mass @ diff)
+
+    def compute_loss(self, mu, q0_true, r, weights=[0.5, 0.5]):
+        loss_r = self.compute_loss_r(q0_true, r)
+        loss_p = self.compute_loss_p(mu, q0_true, r)
+
+        return weights[0] * loss_r + weights[1] * loss_p
+
+    def visualize(self, mu, q0, file_name):
+        # visualization of the results
+        f = self.get_f(mu)
+        q_f = self.swp.sweep(f)
+
+        q = q_f + q0
+        p = self.swp.sweep_transpose(self.face_mass @ q)
+
+        # post process velocity
+        proj_q = pg.eval_at_cell_centers(self.mdg, pg.RT0(self.keyword))
+        cell_q = (proj_q @ q).reshape((3, -1), order="F")
+
+        # post process pressure
+        proj_p = pg.eval_at_cell_centers(self.mdg, pg.PwConstants(self.keyword))
+        cell_p = proj_p @ p
+
+        # save the solutions to be exported in the data dictionary of the mdg
+        for _, data in self.mdg.subdomains(return_data=True):
+            pp.set_solution_values("cell_q", cell_q, data, 0)
+            pp.set_solution_values("cell_p", cell_p, data, 0)
+
+        # export the solutions
+        save = pp.Exporter(self.mdg, file_name)
+        save.write_vtu(["cell_q", "cell_p"])
