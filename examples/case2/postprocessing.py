54bc049abec1242ea80f3517edf17411e80418f4
Nicola Rares Franco <franco.nicola.rares@gmail.com>
2023-07-08 15:47:11 +0200

    Now assumes a collection of ROM solutions is available and automatically opens them in Paraview

--- examples/case2/postprocessing.py
+++ examples/case2/postprocessing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pygeon as pg
 import porepy as pp
+import sys
+import os
 
 
 from main import SamplerSB
@@ -8,9 +10,10 @@ from setup import create_data, fracture_grid
 
 
 if __name__ == "__main__":
-    filename = "snapshots.npz"  # input("Filename: ")
+    filename =  "ROMoutputs_case2.npz"
     obj = np.load(filename)
     mu, q0, step_size = obj["mu"], obj["q0"], obj["h"]
+    mu, q0 = np.atleast_2d(mu), np.atleast_2d(q0)
 
     mesh_kwargs = {"mesh_size_frac": step_size, "mesh_size_min": step_size}
     mdg = fracture_grid(mesh_kwargs)
@@ -22,6 +25,10 @@ if __name__ == "__main__":
     sampler = SamplerSB(mdg, keyword)
 
     # pos = 0 is used in the paper
-    pos = 0
-    print(mu[pos])
+    pos = int(sys.argv[1])
+    aux = ("%.3f, "*len(mu[pos]))[:-2] % tuple(mu[pos])
+    print("Parameter values: [%s]" % aux)
     sampler.visualize(mu[pos], q0[pos], "sol")
+    print("Opening in Paraview...\n")
+    os.system("sol_2.vtu")
+    print("... done.")
